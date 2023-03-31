3c84573f4be61713c5dcc6d337010ae4353c9715
CouffignalCedric <cedric.couffignal.sncf@gmail.com>
2023-03-31 10:35:22 +0200

    test category

--- src/main/java/com/back/projet3/entity/Category.java
+++ src/main/java/com/back/projet3/entity/Category.java
@@ -4,32 +4,22 @@ import java.sql.Timestamp;
 import java.util.*;
 
 import javax.persistence.*;
+import javax.persistence.Enumerated;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
 
 @Data
-@Entity //Table Category
-@Table(name="category")
+@Entity // Table Category
+@Table(name = "category")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private boolean fruits;
-    private boolean vegetables;
-    private boolean tin_can_jars;
-    private boolean other;
-    private boolean batch;
+    private String genre;
 
     @OneToMany(mappedBy = "category")
-    private List<Announcement> category;
-
-
-
-
-
-
-
+    private List<Announcement> listCategory;
 
 }
---
+++ src/main/resources/database.sql
@@ -0,0 +1,4 @@
+USE barter;
+
+ INSERT INTO category (genre)
+ VALUES ('fruits'), ('vegetables'), ('tin_can_jars'),('batch');

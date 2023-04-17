83968c2271c24f45c4e02a6badfe3af9337ea6b9
Apollo02112022 <nicolas.fuente@sncf.fr>
2023-04-17 14:56:59 +0200

    fourth commit

--- src/main/java/com/back/projet3/controller/UserController.java
+++ src/main/java/com/back/projet3/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.back.projet3.controller;
 
-
 import java.io.IOException;
 import java.util.List;
 
@@ -15,148 +14,109 @@ import com.back.projet3.entity.User;
 import com.back.projet3.repository.UserRepository;
 import com.back.projet3.util.ImageUtil;
 
-
 @RestController
 
-@RequestMapping("/api") 
+@RequestMapping("/api")
 
 @CrossOrigin(origins = "http://localhost:4200")
 
 public class UserController {
 
-@Autowired
-private UserRepository userRepository;     
+    @Autowired
+    private UserRepository userRepository;
 
-// Création d'un nouvel utilisateur dans la base de données via le formulaire d'inscription.
+    // Création d'un nouvel utilisateur dans la base de données via le formulaire
+    // d'inscription.
 
-@PostMapping("/signup")
-public ResponseEntity<?> createUser(@ModelAttribute UserDto userDto) {
-    User user = new User();
-    System.out.println("@@@@@@@@@" + userDto.getPassword());
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    String encodedPassword = encoder.encode(userDto.getPassword());
-    user.setPassword(encodedPassword);
+    @PostMapping("/signup")
+    public ResponseEntity<?> createUser(@ModelAttribute UserDto userDto) {
 
-    if(userRepository.findByMail(userDto.getMail()) != null) {
-        return new ResponseEntity<>("Un utilisateur avec l'adresse e-mail " + userDto.getMail() + " existe déjà.", HttpStatus.CONFLICT);
-    } 
+        User user = new User();
 
-    if(userRepository.findByUsername(userDto.getUsername()) != null) {
-        return new ResponseEntity<>("Un utilisateur avec le nom d'utilisateur " + userDto.getUsername() + " existe déjà.", HttpStatus.CONFLICT);
+        // System.out.println("@@@@@@@@@@" + userDto.getPassword());
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(userDto.getPassword());
+        user.setPassword(encodedPassword);
+
+        String lastname = userDto.getLastname();
+        user.setLastname(lastname);
+        String firstname = userDto.getFirstname();
+        user.setFirstname(firstname);
+        String username = userDto.getUsername();
+        user.setUsername(username);
+        String city = userDto.getCity();
+        user.setCity(city);
+        int county = userDto.getCounty();
+        user.setCounty(county);
+        String mail = userDto.getMail();
+        user.setMail(mail);
+
+        if (userRepository.findByMail(userDto.getMail()) != null) {
+            return new ResponseEntity<>("Un utilisateur avec l'adresse e-mail " + userDto.getMail() + " existe déjà.",
+                    HttpStatus.CONFLICT);
+        }
+
+        if (userRepository.findByUsername(userDto.getUsername()) != null) {
+            return new ResponseEntity<>(
+                    "Un utilisateur avec le nom d'utilisateur " + userDto.getUsername() + " existe déjà.",
+                    HttpStatus.CONFLICT);
+        }
+
+        byte[] pictureInByteForm;
+
+        try {
+            pictureInByteForm = ImageUtil.compressImage(userDto.getPicture().getBytes());
+            user.setPicture(pictureInByteForm);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
-    byte[] pictureInByteForm;
-    try {
-        pictureInByteForm = ImageUtil.compressImage(userDto.getPicture().getBytes());
-        user.setPicture(pictureInByteForm);
-    } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+    // Récupération de l'image d'un utilisateur.
+
+    // A modifier pour faire une connection sécurisé
+    @PostMapping("/login") // api/login POST Permet la connexion
+    public User loginUser(User user) {
+
+        return user;
     }
-    
 
-    userRepository.save(user);
-    return new ResponseEntity<>(user, HttpStatus.CREATED);
-}
+    // A modifier pour faire une déconnection sécurisé
+    @PostMapping("/logout") // api/logout POST Permet la déconnexion
+    public User logoutUser(User user) {
 
-//A modifier pour faire une connection sécurisé
-@PostMapping("/login") // api/login POST Permet la connexion
-public User loginUser(User user){
+        return user;
+    }
 
-    return user;
-}
+    @GetMapping("/users") // api/users GET Liste des utilisateurs
+    public List<User> findAllUser() {
 
-//A modifier pour faire une déconnection sécurisé
-@PostMapping("/logout") // api/logout POST Permet la déconnexion
-public User logoutUser(User user){
+        return userRepository.findAll();
+    }
 
-    return user;
-}
+    @GetMapping("/users/{id}/profil") // api/users/{id}/profil GET Détails d’un utilisateur
+    public User findUser(@PathVariable Long id) {
 
-@GetMapping("/users") // api/users GET Liste des utilisateurs
-public List<User> findAllUser(){
+        return userRepository.findById(id).get();
+    }
 
-    return userRepository.findAll();
-}
+    @PutMapping("/users/{id}/profil") // api/users/:usersId PUT Mettre à jours un utilisateur
+    public User UpdateUser(@PathVariable Long id, @RequestBody User user) {
 
-@GetMapping("/users/{id}/profil") // api/users/{id}/profil GET Détails d’un utilisateur
-public User findUser(@PathVariable Long id){
+        User userToUpdate = userRepository.findById(id).get();
+        userToUpdate.setPassword(user.getPassword());
+        userToUpdate.setMail(user.getMail());
+        return userRepository.save(userToUpdate);
+    }
 
-    return userRepository.findById(id).get();
-}
-
-@PutMapping("/users/{id}/profil") // api/users/:usersId PUT Mettre à jours un utilisateur
-public User UpdateUser(@PathVariable Long id, @RequestBody User user){
-
-    User userToUpdate = userRepository.findById(id).get();
-    userToUpdate.setPassword(user.getPassword());
-    userToUpdate.setMail(user.getMail());
-    return userRepository.save(userToUpdate);
-}
-
-@DeleteMapping("/users/{id}/profil") //api/users/:usersId DELETE supprime un utilisateur
-public boolean deleteUser(@PathVariable Long id){
-   userRepository.deleteById(id);
-   return true;
-}
+    @DeleteMapping("/users/{id}/profil") // api/users/:usersId DELETE supprime un utilisateur
+    public boolean deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+        return true;
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// @PostMapping("/signup")
-// public ResponseEntity<?> createUser(@RequestBody User user) {
-//     // Vérifier si l'utilisateur existe déjà:
-//     if (userRepository.findByMail(user.getMail()) == null) {
-//         userRepository.save(user);
-//         return ResponseEntity.ok("Utilisateur créé avec succès.");
-        
-//     }
-//     // Enregistrer l'utilisateur dans la base de données:
-    
-//     return ResponseEntity.badRequest().body("Cet utilisateur existe déjà.");
-// } 
-
-
-
-// @PostMapping("/signup")
-// public ResponseEntity<?> createUser(@RequestParam("file") MultipartFile file, @ModelAttribute User user) throws IOException {
-//     // Enregistre la photo dans le système de fichiers
-//     String filename = file.getOriginalFilename();
-//     Path filepath = Paths.get("uploads", filename);
-//     Files.copy(file.getInputStream(), filepath);
-
-//     // Stocke l'URL de la photo dans la propriété picture de l'objet User
-//     user.setPicture("/uploads/" + filename);
-
-//     // Encode le mot de passe et enregistre l'utilisateur dans la base de données
-//     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//     String encodedPassword = encoder.encode(user.getPassword());
-//     user.setPassword(encodedPassword);
-
-//     if(userRepository.findByMail(user.getMail()) != null) {
-//         return new ResponseEntity<>("Un utilisateur avec l'adresse e-mail " + user.getMail() + " existe déjà.", HttpStatus.CONFLICT);
-//     } 
-
-//     if(userRepository.findByUsername(user.getUsername()) != null) {
-//         return new ResponseEntity<>("Un utilisateur avec le nom d'utilisateur " + user.getUsername() + " existe déjà.", HttpStatus.CONFLICT);
-//     }
-
-//     userRepository.save(user);
-//     return new ResponseEntity<>(user, HttpStatus.CREATED);
-// }
-
-// @PostMapping("/signup")
-// public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
-//   // votre code pour enregistrer le fichier dans la base de données
-//   return ResponseEntity.ok().build();
-// }
